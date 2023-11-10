b26ebec573c32adb1dea1d97dc13db780ed0fbaa
Reza Alipour <56475612+Reza-Alipour@users.noreply.github.com>
2023-11-10 14:30:04 +0330

    Fix a minor issue

--- main.py
+++ main.py
@@ -549,7 +549,8 @@ if __name__ == "__main__":
                 }
             },
         }
-        callbacks_cfg = lightning_config.callbacks or OmegaConf.create()
+        # callbacks_cfg = lightning_config.callbacks or OmegaConf.create()
+        callbacks_cfg = lightning_config.OmegaConf.create()
         callbacks_cfg = OmegaConf.merge(default_callbacks_cfg, callbacks_cfg)
         trainer_kwargs["callbacks"] = [instantiate_from_config(callbacks_cfg[k]) for k in callbacks_cfg]
 
